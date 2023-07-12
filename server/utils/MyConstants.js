2524960ff91b7e7479b7f851618beb4244807d51
sennie252 <sennie252@gmail.com>
2023-07-12 08:44:41 +0700

    aaaa

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'sang.xpxdgh3.mongodb.net',
+    DB_USER: 'thanhsang2507',
+    DB_PASS: 'Admins123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'sennie252@gmail.com',
+    EMAIL_PASS: 'iahjsaecvvzmijnn',
+    JWT_SECRET: '123456',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
